e7c02feb33a60234b67a4f0283d1a4248900c00b
Chang Liu <50190610+Verg-Avesta@users.noreply.github.com>
2022-04-26 23:05:23 +0800

    Add files via upload

---
+++ models_mae_up.py
@@ -0,0 +1,146 @@
+from functools import partial
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from timm.models.vision_transformer import PatchEmbed, Block
+
+from util.pos_embed import get_2d_sincos_pos_embed
+
+class SupervisedMAE(nn.Module):
+    def __init__(self, img_size=384, patch_size=16, in_chans=3,
+                 embed_dim=1024, depth=24, num_heads=16,
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        super().__init__()
+
+        # --------------------------------------------------------------------------
+        # MAE encoder specifics
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches , embed_dim), requires_grad=False)  # fixed sin-cos embedding
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+        # --------------------------------------------------------------------------
+
+        # --------------------------------------------------------------------------
+        # MAE decoder specifics
+        self.decode_head0 = nn.Sequential(
+            nn.Conv2d(embed_dim, 256, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(8, 256),
+            nn.ReLU(inplace=True)
+        )
+        self.decode_head1 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(8, 256),
+            nn.ReLU(inplace=True)
+        )
+        self.decode_head2 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(8, 256),
+            nn.ReLU(inplace=True)
+        )
+        self.decode_head3 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.GroupNorm(8, 256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 1, kernel_size=1, stride=1)
+        )  
+    
+        # --------------------------------------------------------------------------
+
+        self.norm_pix_loss = norm_pix_loss
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=False)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    def forward_encoder(self, x):
+        # embed patches
+        x = self.patch_embed(x)
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        return x
+
+    def forward_decoder(self, x):
+        
+        n, hw, c = x.shape
+        h = w = int(math.sqrt(hw))
+        x = x.transpose(1, 2).reshape(n, c, h, w)
+
+        x = F.interpolate(
+                        self.decode_head0(x), size=x.shape[-1]*2, mode='bilinear', align_corners=False)
+        x = F.interpolate(
+                        self.decode_head1(x), size=x.shape[-1]*2, mode='bilinear', align_corners=False)
+        x = F.interpolate(
+                        self.decode_head2(x), size=x.shape[-1]*2, mode='bilinear', align_corners=False)
+        x = F.interpolate(
+                        self.decode_head3(x), size=x.shape[-1]*2, mode='bilinear', align_corners=False)
+        x = x.squeeze(-3)
+
+        return x
+
+    def forward(self, imgs):
+        with torch.no_grad():
+            latent = self.forward_encoder(imgs)
+        pred = self.forward_decoder(latent)  # [N, h, w]
+        return pred
+
+
+def mae_vit_base_patch16_dec512d8b(**kwargs):
+    model = SupervisedMAE(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def mae_vit_large_patch16_dec512d8b(**kwargs):
+    model = SupervisedMAE(
+        patch_size=16, embed_dim=1024, depth=24, num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+def mae_vit_huge_patch14_dec512d8b(**kwargs):
+    model = SupervisedMAE(
+        patch_size=14, embed_dim=1280, depth=32, num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
+
+# set recommended archs
+mae_vit_base_patch16 = mae_vit_base_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+mae_vit_large_patch16 = mae_vit_large_patch16_dec512d8b  # decoder: 512 dim, 8 blocks
+mae_vit_huge_patch14 = mae_vit_huge_patch14_dec512d8b  # decoder: 512 dim, 8 blocks
